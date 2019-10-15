9ea12a74fbb4f14d7094775c553d90aed84daa81
JaroslawCzerwinski <44581966+JaroslawCzerwinski@users.noreply.github.com>
2019-10-15 17:43:03 +0200

    add new content implementation

--- src/pl/javastart/weekop/service/DiscoveryService.java
+++ src/pl/javastart/weekop/service/DiscoveryService.java
@@ -1,5 +1,29 @@
 package pl.javastart.weekop.service;
 
-public class DiscoveryService {
+import java.util.Date;
+import java.sql.Timestamp;
 
+import pl.javastart.weekop.dao.DAOFactory;
+import pl.javastart.weekop.dao.DiscoveryDAO;
+import pl.javastart.weekop.model.Discovery;
+import pl.javastart.weekop.model.User;
+
+public class DiscoveryService {
+	public void addDiscovery(String name, String desc, String url, User user) {
+		Discovery discovery = createDiscoveryObject(name, desc, url, user);
+		DAOFactory factory = DAOFactory.getDAOFactory();
+		DiscoveryDAO discoveryDao = factory.getDiscoveryDAO();
+		discoveryDao.create(discovery);
+	}
+
+	private Discovery createDiscoveryObject(String name, String desc, String url, User user) {
+		Discovery discovery = new Discovery();
+		discovery.setName(name);
+		discovery.setDescription(desc);
+		discovery.setUrl(url);
+		User userCopy = new User(user);
+		discovery.setUser(userCopy);
+		discovery.setTimestamp(new Timestamp(new Date().getTime()));
+		return discovery;
+	}
 }
